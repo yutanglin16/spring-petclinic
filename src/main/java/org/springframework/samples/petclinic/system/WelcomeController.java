240d5020e8e12ffa397e05bfd476594e5aabd4c8
Jason Freeberg <jr.freeberg@comcast.net>
2019-12-09 11:27:04 -0800

    Add HTML table and the API endpoint

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -17,14 +17,84 @@
 package org.springframework.samples.petclinic.system;
 
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.samples.petclinic.vet.Vet;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 class WelcomeController {
 
+    private final String RESOURCE_SERVER_BASE_URI = "https://resource-server-demo.azurewebsites.net";
+
     @GetMapping("/")
-    public String welcome() {
+    public String welcome(Map<String, Object> model) {
+        List<Vet> vets = getCurrentVetsOnCall();
+        model.put("vets", vets);
         return "welcome";
+    }
+
+    private List<Vet> getCurrentVetsOnCall() {
+        final String endpoint = this.RESOURCE_SERVER_BASE_URI+"/api/vets/on-call";
+
+        try {
+            final KeyStore keyStore = this.getKeystore();
+            TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+            SSLContext sslContext = SSLContextBuilder
+                    .create()
+                    .loadKeyMaterial(keyStore,  "changeit".toCharArray())
+                    .loadTrustMaterial(keyStore, acceptingTrustStrategy)
+                    .build();
+            HttpClient client = HttpClients
+                    .custom()
+                    .setSSLContext(sslContext)
+                    .build();
+
+            RestTemplate template = new RestTemplate();
+            template.setRequestFactory(new HttpComponentsClientHttpRequestFactory(client));
+
+            HttpEntity<?> httpEntity = new HttpEntity(null, new HttpHeaders());
+
+            List<?> response = template
+                    .exchange(new URI(endpoint), HttpMethod.GET, httpEntity, List.class)
+                    .getBody();
+
+            List<Vet> vetsOnCall = (List<Vet>) response;
+
+            return vetsOnCall;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private KeyStore getKeystore() throws KeyStoreException, NoSuchAlgorithmException,
+            IOException, CertificateException {
+        KeyStore keyStore = KeyStore.getInstance("jks");
+        keyStore.load(
+                new FileInputStream(System.getenv("JAVA_HOME")+"/lib/security/client.jks"),
+                "changeit".toCharArray());
+        return keyStore;
     }
 }
